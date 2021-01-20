72ce323aca9534606105aeb6e11637416978d193
everaldobass <everaldobass@gmail.com>
2021-01-19 23:14:27 -0300

    Atualizando o curso de SGBD

---
+++ aula01.sql
@@ -0,0 +1,26 @@
+/*
+Comandos DDL
+Data Definition Languagem - DDL (Linguagem de Definição de Dados)
+
+Sintaxe
+ Objetivo criar um banco de dados
+ CREATE DATABASE<nome-do-banco>.
+*/
+CREATE DATABASE Biblioteca;
+/*
+Selecionar o banco que será usado
+USE <nome-do-banco>
+*/
+USE Biblioteca;
+
+/*
+Deletar um banco de dados
+DROP DATABASE <nome-banco-de-dados>
+*/
+DROP DATABASE Biblioteca;
+
+/*
+Não é possivel deletar um banco de dados em uso.
+Neste caso deve fechar o banco de dados em uso com a abertura de outro banco de dados. Ex. USE banco.
+
+*/
